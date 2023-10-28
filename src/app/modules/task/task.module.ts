1315e96c49a38e0e94020ee878b0b5f6615c6026
Mateusz Szlachetko <mateusz.szlachetko@gmail.com>
2023-10-28 14:03:13 +0200

    fix: add loading of tasks to OnInit to prevent bug of not firing tabChange event, after initial load and redirection

--- src/app/modules/task/task.module.ts
+++ src/app/modules/task/task.module.ts
@@ -3,7 +3,8 @@ import {CommonModule} from '@angular/common';
 import {TaskComponent} from './task/task.component';
 import {TaskRoutingModule} from "./task-routing.module";
 import {MatTabsModule} from "@angular/material/tabs";
-import { TaskCardComponent } from './components/task-card/task-card.component';
+import {TaskCardComponent} from './components/task-card/task-card.component';
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 
 
 @NgModule({
@@ -14,7 +15,8 @@ import { TaskCardComponent } from './components/task-card/task-card.component';
   imports: [
     CommonModule,
     TaskRoutingModule,
-    MatTabsModule
+    MatTabsModule,
+    MatProgressSpinnerModule
   ]
 })
 export class TaskModule {
--- src/app/modules/task/task/task.component.ts
+++ src/app/modules/task/task/task.component.ts
@@ -1,16 +1,14 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {CategoryService} from "../../../core/services/category.service";
-import {Observable, Subject, takeUntil} from "rxjs";
+import {Subject, takeUntil} from "rxjs";
 import {CategoryInterface} from "../../../core/interfaces/category.interface";
-import {MatTabChangeEvent} from "@angular/material/tabs";
 import {TaskInterface} from "../../../core/interfaces/task.interface";
 import {TaskService} from "../../../core/services/task.service";
-import {Router} from "@angular/router";
 
-// export interface TasksTab {
-//   category: CategoryInterface,
-//   content: TaskInterface[]
-// }
+export interface TasksTab {
+  category: CategoryInterface,
+  content: TaskInterface[]
+}
 
 @Component({
   selector: 'app-task',
@@ -19,36 +17,31 @@ import {Router} from "@angular/router";
 })
 export class TaskComponent implements OnInit, OnDestroy {
   destroy$: Subject<void> = new Subject();
-  categories: CategoryInterface[] = [];
-  tasks: TaskInterface[] = [];
+  tabs: TasksTab[] = [];
+  isLoading: boolean = false;
 
   constructor(private categoryService: CategoryService, private taskService: TaskService) {
   }
 
   ngOnInit() {
-    console.log("init")
+    this.isLoading = true;
     this.categoryService.getCategories().pipe(takeUntil(this.destroy$))
       .subscribe((data) => {
-        this.categories = data;
-        console.log(this.categories)
-        if (data.length !== 0)
-          this.taskService.getTasksByCategoryId(data[0].id).pipe(takeUntil(this.destroy$)).subscribe(data => {
-            this.tasks = data;
-          })
-      })
+        if (data.length !== 0) {
+          data.forEach((category, index) => {
+            this.tabs.push({category: category, content: []})
+            this.taskService.getTasksByCategoryId(category.id).pipe(takeUntil(this.destroy$)).subscribe(data => {
+              this.isLoading = false;
+              this.tabs[index].content = data;
+            })
+          });
+        }
+      });
+    // consider - loading only first tab, load other on change event
   }
 
   ngOnDestroy() {
     this.destroy$.next();
     this.destroy$.complete();
-  }
-
-  onTabChange($event: MatTabChangeEvent) {
-    const i = $event.index;
-    const category = this.categories[i];
-
-    this.taskService.getTasksByCategoryId(category.id).pipe(takeUntil(this.destroy$)).subscribe(data => {
-      this.tasks = data;
-    })
   }
 }
