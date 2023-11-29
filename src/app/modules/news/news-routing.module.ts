893be9555bd3f74acaf912eb4ff33f50fd8b7188
Mateusz Szlachetko <mateusz.szlachetko@gmail.com>
2023-11-29 14:51:45 +0100

    feat: add admin guard to news module components

--- src/app/modules/news/news-routing.module.ts
+++ src/app/modules/news/news-routing.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {NewsComponent} from "./news/news.component";
 import {NewsFormComponent} from "./components/news-form/news-form.component";
+import {adminGuard} from "../../core/guards/admin.guard";
 
 
 const routes: Routes = [
@@ -9,10 +10,10 @@ const routes: Routes = [
     path: '', component: NewsComponent, children: [],
   },
   {
-    path: 'add', component: NewsFormComponent, children: []
+    path: 'add', component: NewsFormComponent, children: [], canActivate: [adminGuard]
   },
   {
-    path: 'edit/:id', component: NewsFormComponent, children: []
+    path: 'edit/:id', component: NewsFormComponent, children: [], canActivate: [adminGuard]
   }
 ];
 
